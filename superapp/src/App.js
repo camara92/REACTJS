4537852a71998e8749ed718388476a9f1253c2ba
camara92 <88945535+camara92@users.noreply.github.com>
2022-10-04 14:22:10 +0200

    onclick condition

--- superapp/src/App.js
+++ superapp/src/App.js
@@ -11,9 +11,18 @@ class App extends React.Component{
     constructor(props){
         super(props)
         this.state= {title: 'Coucou les amis ! '}
+        this.state = {show:  false}
     }
     render(){
-        return(<h1 className="text-center bg-dark text-white "> {this.state.title } </h1> )
+        return(<div> 
+            <button className='bg-purple text-white rounded py-2 px-3 ' onClick={()=>this.setState({show : true})}>
+                Click 
+            </button>
+            {
+                this.state.show ? 
+                <image className='w-40  m-5 h-25'  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrrX_9iANDm2mE-gZvxVo7xN0-hqwhf-yJmw&usqp=CAU' alt='image au click ' /> : null
+            }
+        </div> )
     }
 }
 

296f787aa9f41a0cb9c617971d3e3088af4b21a9
camara92 <88945535+camara92@users.noreply.github.com>
2022-10-04 14:01:04 +0200

    composant en react js

--- superapp/src/App.js
+++ superapp/src/App.js
@@ -1,9 +1,20 @@
 import './css/app.css'
 
-function App({title }){
+import  'bootstrap/dist/css/bootstrap.min.css' ;
+import React from 'react';
+// function App({title }){
 
-    return(<h1 className="heading"> {title} </h1> )
+//     return(<h1 className="text-center bg-dark text-white "> {title} </h1> )
     
+//     }
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state= {title: 'Coucou les amis ! '}
     }
+    render(){
+        return(<h1 className="text-center bg-dark text-white "> {this.state.title } </h1> )
+    }
+}
 
 export default App; 
